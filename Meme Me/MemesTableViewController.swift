f3c3ff6a85478c2d1f07c48217b4c3d220674c23
minaatefmaf <minaatefmaf@gmail.com>
2015-05-25 21:02:16 +0300

    Try to add the tableViewController (1).

--- Meme Me/MemesTableViewController.swift
+++ Meme Me/MemesTableViewController.swift
@@ -8,6 +8,36 @@
 
 import UIKit
 
-class MemesTableViewController: UIViewController {
+class MemesTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var memes: [Meme]!
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Retrieve the sent memes.
+        let object = UIApplication.sharedApplication().delegate
+        let appDelegate = object as! AppDelegate
+         memes = appDelegate.memes
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.memes.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("MemeTableCell") as! UITableViewCell
+        let villain = self.memes[indexPath.row]
+        
+        // Set the name and image
+        cell.textLabel?.text = memes[indexPath.row].topText
+        //cell.imageView?.image = UIImage(named: memes[indexPath.row].memedImage)
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
     
 }
