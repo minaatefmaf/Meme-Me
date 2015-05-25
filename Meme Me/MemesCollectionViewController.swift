07c6d02635b64b885ff69eef5f97535d20696481
minaatefmaf <minaatefmaf@gmail.com>
2015-05-25 20:35:38 +0300

    Add the tab bar controller, and MemesTableViewController & MemescollectionViewController.

---
+++ Meme Me/MemesCollectionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MemesCollectionViewController.swift
+//  Meme Me
+//
+//  Created by Mina Atef on 5/25/15.
+//  Copyright (c) 2015 minaatefmaf. All rights reserved.
+//
+
+import UIKit
+
+class MemesCollectionViewController: UIViewController {
+    
+}
