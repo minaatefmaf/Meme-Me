de6d0f0c4055f47717c91a6a8ce3a978d6e053a7
minaatefmaf <minaatefmaf@gmail.com>
2015-05-25 19:19:35 +0300

    Create a Meme struct.

---
+++ Meme Me/Meme.swift
@@ -0,0 +1,17 @@
+//
+//  Meme.swift
+//  Meme Me
+//
+//  Created by Mina Atef on 5/25/15.
+//  Copyright (c) 2015 minaatefmaf. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    var topText: String?
+    var bottonText: String?
+    var image: UIImage
+    var memedImage: UIImage
+}
