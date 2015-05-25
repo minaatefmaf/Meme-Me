f40dee90b7ec50c0d97251b472b1b7cf4c714f65
minaatefmaf <minaatefmaf@gmail.com>
2015-05-25 17:12:18 +0300

    Resolve a bug with NSStrokeWidthAttributeName (Specify negative values to stroke and fill the text). Clean the code a little.

--- Meme Me/DefaultTextAttributes.swift
+++ Meme Me/DefaultTextAttributes.swift
@@ -16,6 +16,7 @@ struct DefaultTextAttributes {
         NSStrokeColorAttributeName: UIColor.blackColor(),
         NSForegroundColorAttributeName: UIColor.whiteColor(),
         NSFontAttributeName: UIFont(name: "HelveticaNeue-CondensedBlack", size: 30)!,
+        // Specify positive values to change the stroke width alone. Specify negative values to stroke and fill the text.
         NSStrokeWidthAttributeName: -3.0
     ]
 }
